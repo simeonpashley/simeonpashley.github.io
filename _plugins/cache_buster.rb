d2d990e7c90540e52cae0f945d65a8f3b70578dd
Simeon Pashley <simeon@pashley.org>
2024-12-12 20:27:44 +0000

    simple cache buster for cloudflare

--- _plugins/cache_buster.rb
+++ _plugins/cache_buster.rb
@@ -2,9 +2,12 @@ module Jekyll
   module CacheBuster
     require 'digest/md5'
     def cache_buster(file_name)
-      md5_digest = Digest::MD5.hexdigest(File.read(File.join('.', file_name)))
+      # repo = Grit::Repo.new(Dir.pwd)
+      # rev = repo.log("main", "#{Dir.pwd}#{@resource}").first.id_abbrev
+      # suffix = Digest::MD5.hexdigest(File.read(File.join('.', file_name)))
+      suffix = Time.now.to_i
       processed_file_name = file_name.gsub(%r{/_site/assets/}, '/assets/')
-      [processed_file_name, '?v=', md5_digest].join
+      [processed_file_name, '?v=', suffix].join
     end
   end
 end
