0be36196952f5bba81143c455fce69d806ac9537
Simeon Pashley <simeon@pashley.org>
2024-12-12 17:59:14 +0000

    bodge in a cache buster for the CSS files

--- _plugins/cache_buster.rb
+++ _plugins/cache_buster.rb
@@ -2,7 +2,9 @@ module Jekyll
   module CacheBuster
     require 'digest/md5'
     def cache_buster(file_name)
-      [file_name, '?v=', Digest::MD5.hexdigest(File.read(File.join('./', file_name)))].join
+      md5_digest = Digest::MD5.hexdigest(File.read(File.join('.', file_name)))
+      processed_file_name = file_name.gsub(%r{/_site/assets/}, '/assets/')
+      [processed_file_name, '?v=', md5_digest].join
     end
   end
 end
